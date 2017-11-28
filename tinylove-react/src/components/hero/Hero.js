eeb94be19ee05869a109a9abfe99f822e08c39e6
Camila Belo <camilaibs@gmail.com>
2017-11-28 21:39:28 -0200

    Renomeado os atributos class do Hero

--- tinylove-react/src/components/hero/Hero.js
+++ tinylove-react/src/components/hero/Hero.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './Hero.css';
 
 const Hero = (props) => (
-  <header class="hero">
-        <h1 class="hero__title">{props.coisa}</h1>
-        <h2 class="hero__subtitle">{props.subtitle}</h2>
-        <p class="hero__text">
+  <header className="hero">
+        <h1 className="hero__title">{props.coisa}</h1>
+        <h2 className="hero__subtitle">{props.subtitle}</h2>
+        <p className="hero__text">
           {props.text}
         </p>
   </header>
